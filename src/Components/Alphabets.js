53982f420a9b46bbab3745722cc9c1a571887d03
abriyusif <abriyusif20@gmail.com>
2022-06-13 15:45:14 +0300

    linked the cards to their various components

---
+++ src/Components/Alphabets.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Alphabets = () => {
+  return (
+    <div>
+      <h1>Alphabets page</h1>
+    </div>
+  )
+}
+
+export default Alphabets
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Footer from "./Footer";
+import {Link} from 'react-router-dom';
 
 import "../styles/Home.css";
 
@@ -22,68 +23,85 @@ const MidHome = () => {
       name: "Alphabets",
       color: "green",
       id: 1,
+      url:"/alphabets"
     },
     {
       name: "Shapes",
       color: "yellow",
       id: 2,
+      url:"/shapes"
     },
     {
       name: "Colors",
       color: "purple",
       id: 3,
+        url:"/colors"
     },
     {
-      name: "Days of the week",
-      color: "white",
+    name: "Days of the week",
+    color: "white",
       id: 10,
-    },
+    url: "/days"
+},
     {
       name: "Months",
       color: "blue",
       id: 4,
+      url:"/months"
     },
     {
       name: "Animals",
       color: "red",
       id: 5,
+      url:"/animals"
     },
     {
       name: "Fruits",
       color: "orange",
       id: 6,
+      url:"/fruits"
     },
     {
       name: "Parts of the body",
       color: "brown",
       id: 7,
+      url:"/body"
     },
     {
       name: "Arithmetic",
       color: "pink",
       id: 8,
+      url:"/arithmetic"
     },
     {
       name: "Parts of a Computer",
       color: "dodgerblue",
       id: 9,
+      url:"/computer"
     },
     {
-      name: "",
+      //not important here
+      name: "Trees",
       color: "dodgerblue",
       id: 11,
+      url:"/trees"
     },
     {
-      name: "",
+    //not important here
+      name: "Chairs",
       color: "dodgerblue",
       id: 12,
+      url:"/chairs"
     },
   ];
   return (
     <div className="mid-home">
       <div className="buttons-container">
         {buttonsArray.map((object) => (
-          <Topic key={object.id} object={object} />
+          <Link to={object.url}>
+
+            <Topic key={object.id} object={object} />
+          </Link>
         ))}
       </div>
       
