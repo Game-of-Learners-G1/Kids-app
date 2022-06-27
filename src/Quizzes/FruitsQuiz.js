354688be12a706fad1436c42ba30188d416383b4
chuol tayar <93642845+chuoltiap@users.noreply.github.com>
2022-06-27 13:35:51 +0300

    Fruits edited

--- src/Quizzes/FruitsQuiz.js
+++ src/Quizzes/FruitsQuiz.js
@@ -19,39 +19,79 @@ const FruitsQuiz = () => {
 			answerOptions: [
 				{ answerText: 'Watermelon', isCorrect: false },
 				{ answerText: 'Mango', isCorrect: false },
-				{ answerText: 'orange', isCorrect: false },
+				{ answerText: 'Orange', isCorrect: false },
 				{ answerText: 'Banana', isCorrect: true },
 			],
 		},
 		{
+			questionText: 'This is an ',
+			image: 'https://media.istockphoto.com/photos/avocados-isolated-on-white-picture-id94929126?k=20&m=94929126&s=612x612&w=0&h=OOKnuKrGI3BA5kK2VikVjBPqdVgtH4xxhHprxxMzh8A=',
+				  answerOptions: [
+					  { answerText: 'Pawpaw', isCorrect: false },
+					  { answerText: 'Avocado', isCorrect: true },
+					  { answerText: 'Orange', isCorrect: false },
+					  { answerText: 'Mango', isCorrect: false },
+				  ],
+			  },
+		{
       questionText: 'how do we call this fruit?',
-      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQt17-7k8oV7lMYzIEofWm6-8SQEngcP14QNA&usqp=CAU',
+      image: 'https://media.istockphoto.com/photos/half-papaya-showing-orange-flesh-and-dark-seeds-green-skin-picture-id146786826?k=20&m=146786826&s=612x612&w=0&h=CSfLYHSLVca0tCthQII1XETJgUz3HyAJ16nzl9rTe6c=',
 			answerOptions: [
-				{ answerText: 'pawpaw', isCorrect: true },
-				{ answerText: 'avocado', isCorrect: false },
-				{ answerText: 'mango', isCorrect: false },
-				{ answerText: 'pineapple', isCorrect: false },
+				{ answerText: 'Pawpaw', isCorrect: true },
+				{ answerText: 'Avocado', isCorrect: false },
+				{ answerText: 'Mango', isCorrect: false },
+				{ answerText: 'Pineapple', isCorrect: false },
 			],
 		},
 		{
       questionText: 'This fruit is called ?',
-      image: 'https://plantvillage-production-new.s3.amazonaws.com/images/pics/000/003/978/original/270435530_a82f19bb63_z.jpg',
+      image: 'https://media.istockphoto.com/photos/isolated-pink-guava-fruits-picture-id1221000031?k=20&m=1221000031&s=612x612&w=0&h=uKMZjrV_5M8jQqYXNClNPxrxdfRe9KoI82QQ-TPkh5Q=',
 			answerOptions: [
-				{ answerText: 'watermelon', isCorrect: false },
+				{ answerText: 'Watermelon', isCorrect: false },
 				{ answerText: 'Guava', isCorrect: true },
-				{ answerText: 'lemon', isCorrect: false },
-				{ answerText: 'mango', isCorrect: false },
+				{ answerText: 'Lemon', isCorrect: false },
+				{ answerText: 'Mango', isCorrect: false },
 			],
     },
+	{
+		questionText: 'Name the fruit below ',
+		image: 'https://media.istockphoto.com/photos/watermelon-picture-id157636928?k=20&m=157636928&s=612x612&w=0&h=4nDJO2G0CkYFM0-LPo_-Keb__X6VRqTxcRxp5NkkFcg=',
+			  answerOptions: [
+				  { answerText: 'Watermelon', isCorrect: true },
+				  { answerText: 'Guava', isCorrect: false },
+				  { answerText: 'Pawpaw', isCorrect: false },
+				  { answerText: 'Mango', isCorrect: false },
+			  ],
+	  },
+	  {
+		questionText: 'What is the name of the fruit below?',
+		image: 'https://media.istockphoto.com/photos/fresh-slices-of-mango-on-a-bed-of-leaves-picture-id168278888?k=20&m=168278888&s=612x612&w=0&h=qFnu2UgMZKfrkCkZfGKbEhCJ2ViokXmel7WSZi_ZbPM=',
+			  answerOptions: [
+				  { answerText: 'Watermelon', isCorrect: false },
+				  { answerText: 'Guava', isCorrect: false },
+				  { answerText: 'Pawpaw', isCorrect: false },
+				  { answerText: 'Mango', isCorrect: true },
+			  ],
+	  },
     
     {
       questionText: 'Name the fruit below',
       image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHRiXjrGJEHnS7THBFnoXvQt20J3oUI49WXw&usqp=CAU',
 			answerOptions: [
-				{ answerText: 'pawpaw', isCorrect: false },
-				{ answerText: 'banana', isCorrect: false },
-				{ answerText: 'guava', isCorrect: false },
-				{ answerText: 'apple', isCorrect: true },
+				{ answerText: 'Pawpaw', isCorrect: false },
+				{ answerText: 'Banana', isCorrect: false },
+				{ answerText: 'Guava', isCorrect: false },
+				{ answerText: 'Apple', isCorrect: true },
+			],
+    },
+	{
+	questionText: 'This is an ?',
+      image: 'https://media.istockphoto.com/photos/tangerine-duo-with-leafs-picture-id182463420?k=20&m=182463420&s=612x612&w=0&h=9oJOMbn4Kki8rAKX9goJ-hUuk3U93ahR3mwSUozaCLA=',
+			answerOptions: [
+				{ answerText: 'Strawberry', isCorrect: false },
+				{ answerText: 'Orange', isCorrect: true },
+				{ answerText: 'Lemon', isCorrect: false },
+				{ answerText: 'Mango', isCorrect: false },
 			],
     },
 
@@ -59,10 +99,10 @@ const FruitsQuiz = () => {
       questionText: 'This fruit is the sweetest, what is the name of the fruit ',
       image: 'https://thinkorg.ap-south-1.linodeobjects.com/uploads/products/meta/qXxX7CCRSUVZg5wLAVvSTPTOrFOoGLzvJHvwQu0V.jpeg',
 			answerOptions: [
-				{ answerText: 'pineapple', isCorrect: true },
-				{ answerText: 'watermelon', isCorrect: false },
-				{ answerText: 'orange', isCorrect: false },
-				{ answerText: 'apple', isCorrect: false },
+				{ answerText: 'Pineapple', isCorrect: true },
+				{ answerText: 'Watermelon', isCorrect: false },
+				{ answerText: 'Orange', isCorrect: false },
+				{ answerText: 'Apple', isCorrect: false },
 			],
 		},
     
