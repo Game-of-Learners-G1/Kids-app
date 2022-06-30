fd29ed03488a1ccc6ea4ee247d398943f4e8b63f
abriyusif <abriyusif20@gmail.com>
2022-06-30 16:32:31 +0300

    added buttons to the payment details

--- src/Components/Mpesa.js
+++ src/Components/Mpesa.js
@@ -1,14 +1,40 @@
 import React from 'react';
 import Footer from './Footer';
+import '../styles/Donate.css'
+import mpesa from '../Images/mpesa.png';
+import equity from '../Images/equity.jpg';
+import kcp from '../Images/kcp.jpg';
 
 const Mpesa = () => {
   return (
-      <div>
-          <h1> Mpesa</h1>
+    <div>
+       <h1> Mpesa</h1>
+      <div className='payDetails'>
+        
 
-          <div>Something else</div>
-          <Footer/>
-    </div>
+      <div className='mpesaDetails'>
+        <button>
+          <img src={mpesa} alt=''/>
+        </button>
+      </div>
+
+      <div className='kcpDetails'>
+        <button>
+          <img src={kcp} alt=''/>
+        </button>
+      </div>
+
+      <div className='equityDetails'>
+        <button>
+          <img src={equity} alt=''/>
+        </button>
+      </div>
+
+  
+      </div>
+      <Footer />
+      
+      </div>
   )
 }
 
