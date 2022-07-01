a7398f043999b1ec0d3f18965949bc708756508d
abriyusif <abriyusif20@gmail.com>
2022-07-01 14:17:34 +0300

    Updates

---
+++ precache-manifest.abb868cc894020aeb8b35f60133c888b.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "94be38a030ab43181b4e",
+    "url": "/kidsapp/static/css/main.a8705081.chunk.css"
+  },
+  {
+    "revision": "94be38a030ab43181b4e",
+    "url": "/kidsapp/static/js/main.a6a11603.chunk.js"
+  },
+  {
+    "revision": "7c9225432f51255cba92",
+    "url": "/kidsapp/static/js/runtime~main.e0830bd6.js"
+  },
+  {
+    "revision": "fea772568c6dce8c18b6",
+    "url": "/kidsapp/static/js/2.bf451d57.chunk.js"
+  },
+  {
+    "revision": "90d54a26fd38c0c96cc8",
+    "url": "/kidsapp/static/js/3.6fa118e9.chunk.js"
+  },
+  {
+    "revision": "2a2c1bb191f251c3a5267b492365790e",
+    "url": "/kidsapp/static/media/instagramlogo.2a2c1bb1.png"
+  },
+  {
+    "revision": "7f1d37c86aa8f6979747a62ca263c759",
+    "url": "/kidsapp/static/media/gmaillogo.7f1d37c8.png"
+  },
+  {
+    "revision": "6844403d8c3c637426b52bf2248ed0d5",
+    "url": "/kidsapp/static/media/linkedinlogo.6844403d.png"
+  },
+  {
+    "revision": "f5a14980f5e713ae914b35f4acc376f7",
+    "url": "/kidsapp/static/media/computer.f5a14980.png"
+  },
+  {
+    "revision": "7802621a137ac0a42b07bdce051e88c6",
+    "url": "/kidsapp/static/media/online-education.7802621a.jpg"
+  },
+  {
+    "revision": "6ab2a1d820966977f8306e44b965e7b9",
+    "url": "/kidsapp/static/media/Students.6ab2a1d8.png"
+  },
+  {
+    "revision": "67b092b5c056cf5f0bf59af4dd4ac13a",
+    "url": "/kidsapp/static/media/liz.67b092b5.png"
+  },
+  {
+    "revision": "454dbb39e9071c88c72ca9e94faed23a",
+    "url": "/kidsapp/static/media/tayar.454dbb39.jpg"
+  },
+  {
+    "revision": "14c6587ff7d38ae48d4ed548732585ff",
+    "url": "/kidsapp/static/media/chan1.14c6587f.jpg"
+  },
+  {
+    "revision": "037fd106535970a795b33dc0e83983da",
+    "url": "/kidsapp/static/media/abri.037fd106.jpg"
+  },
+  {
+    "revision": "559f55999bf15767442d3e22b2f25a2a",
+    "url": "/kidsapp/static/media/cute.559f5599.jpg"
+  },
+  {
+    "revision": "7e21b9661c85d61676143a8ae2c9a73b",
+    "url": "/kidsapp/static/media/phone.7e21b966.jpg"
+  },
+  {
+    "revision": "8695136c90affc54eec528f5df160063",
+    "url": "/kidsapp/index.html"
+  }
+];
